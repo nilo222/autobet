d15654537da562f285b2121645958628bfd55e90
nilo222 <66169137+nilo222@users.noreply.github.com>
2021-11-13 12:00:38 +0000

    Add files via upload

---
+++ config.py
@@ -0,0 +1,18 @@
+token = "1db23b7e99bd0c89853626318cc026a6e9cb98d75d01fa510db5442204ad04b"
+
+coin = "GEM"
+
+"""
+hilo : high or low, hi = high, lo = low
+"""
+
+betset = {
+    "base bet":"0.1",
+    "chance":"60",
+    "if win":"0.85",
+    "if lose":"1.6",
+    "reset if win":"8",
+    "reset if lose":"0",
+    "target profit":"1000",
+    "target balance":"10000"
+}
